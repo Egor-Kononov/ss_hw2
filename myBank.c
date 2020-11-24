d551fb56feb85d98a6bc1396600fd05b49d92d60
Egor <egor.kononov43@gmail.com>
2020-11-24 21:00:56 +0200

    added myBank.c and myBank.h

---
+++ myBank.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include "myBank.h"
+#define SIZE 50
+#define FIRST_ACCOUNT 901
+
+
+double bank[2][bank_accounts] = {0};
+
+int open(double amount){
+    int account_number = -1;
+    for(int i = 0; i < SIZE; i++){
+        if(bank[0][i]== 0){
+           bank[0][i] = 1;
+           bank[1][i] = amount;
+           account_number = FIRST_ACCOUNT+i;
+           return account_number;
+        }
+    }
+return account_number;
+}
+
+double balance(int bankAccount){
+    double current_balance = -1;
+    if(bank[0][bankAccount-FIRST_ACCOUNT]==1){
+        current_balance = bank[1][bankAccount-FIRST_ACCOUNT];
+    }
+    return current_balance;
+}
+
+double deposit(int bankAccount, double amount){
+    double newBalance = -1;
+    if(bank[0][bankAccount-FIRST_ACCOUNT]==1){
+        bank[1][bankAccount-FIRST_ACCOUNT] += amount;
+        newBalance =  bank[1][bankAccount-FIRST_ACCOUNT];
+    }
+    return newBalance;
+}
+
+ double withdraw(int bankAccount, double withdraw){
+     double newBalance = -1;
+     if(bank[0][bankAccount-FIRST_ACCOUNT]==1 && withdraw <= bank[1][bankAccount-FIRST_ACCOUNT] ){ 
+        bank[1][bankAccount-FIRST_ACCOUNT]-= withdraw;
+        newBalance = bank[1][bankAccount-FIRST_ACCOUNT];   
+     }else if(bank[0][bankAccount-FIRST_ACCOUNT]==0){
+         newBalance = -2;
+     }
+     return newBalance;
+ }
+
+ void close(int bankAccount){
+     if(bank[0][bankAccount-FIRST_ACCOUNT]==1){
+         bank[0][bankAccount-FIRST_ACCOUNT]= 0;
+         bank[1][bankAccount-FIRST_ACCOUNT]= 0;
+         printf("This account %d closed successfully!\n", bank_accounts);
+     }else
+     {
+         printf("error: this account already closed!\n");
+     }
+ }
+
+ void interest(double inter){
+      for(int i = 0; i < SIZE; i++){
+          if(bank[0][i]==1){
+              bank[1][i] += (bank[1][i]*(inter/100));
+          }
+      }
+ }
+
+ void print(){
+     for(int i = 0; i < SIZE; i++){
+         if(bank[0][i]==1){
+             printf("account number: %d amount: %0.2lf\n", i+FIRST_ACCOUNT , bank[1][i]);
+         }
+     }
+ }
+
+ void escape(){
+     for(int i = 0; i < SIZE; i++){
+         if(bank[0][i]==1){
+             bank[0][i]=0;
+             bank[1][i]=0;
+         }
+     }
+     printf("all accounts are closed\n");
+ }
+
---
+++ myBank.h
@@ -0,0 +1,16 @@
+#ifndef myBank_h
+#define myBank_h
+#define bank_accounts 50
+
+extern double bank[2][bank_accounts];
+
+int open(double ammount); // if the user typed "O". 
+double balance(int bankAccount); //if the user typed "B".
+double deposit(int bankAccount, double amount); //if the user typed "D".
+double withdraw(int bankAccount, double withdraw); //if the user typed "W". 
+void close(int bankAccount); //if the user typed "C". 
+void interest(double inter); //if the user typed "I". 
+void print(); //if the user typed "P". 
+void escape(); //if the user typed "E".
+
+#endif
